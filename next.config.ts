22fe79f1dbe425fee2f2d86034e77a89a1e24d34
ninthsister <1297234111@qq.com>
2026-06-12 18:15:06 +0800

    修改next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: [
+    "172.23.148.30",
+    "172.23.148.30:3000",
+    "http://172.23.148.30:3000",
+    "http://172.23.148.30:8080",
+  ],
 };
 
 export default nextConfig;
